2c8e1fe8da9e84a4168226b8dc75fc0e6e801323
delakka <del.akka@gmail.com>
2014-07-05 21:49:57 +0200

    formatting

--- main.cpp
+++ main.cpp
@@ -1,62 +1,17 @@
-//=============================================================================================
-// Szamitogepes grafika hazi feladat keret. Ervenyes 2012-tol.          
-// A //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// sorokon beluli reszben celszeru garazdalkodni, mert a tobbit ugyis toroljuk. 
-// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat. 
-// Tilos:
-// - mast "beincludolni", illetve mas konyvtarat hasznalni
-// - faljmuveleteket vegezni (printf is fajlmuvelet!)
-// - new operatort hivni az onInitialization függvényt kivéve, a lefoglalt adat korrekt felszabadítása nélkül 
-// - felesleges programsorokat a beadott programban hagyni
-// - tovabbi kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
-// ---------------------------------------------------------------------------------------------
-// A feladatot ANSI C++ nyelvu forditoprogrammal ellenorizzuk, a Visual Studio-hoz kepesti elteresekrol
-// es a leggyakoribb hibakrol (pl. ideiglenes objektumot nem lehet referencia tipusnak ertekul adni)
-// a hazibeado portal ad egy osszefoglalot.
-// ---------------------------------------------------------------------------------------------
-// A feladatmegoldasokban csak olyan gl/glu/glut fuggvenyek hasznalhatok, amelyek
-// 1. Az oran a feladatkiadasig elhangzottak ES (logikai AND muvelet)
-// 2. Az alabbi listaban szerepelnek:  
-// Rendering pass: glBegin, glVertex[2|3]f, glColor3f, glNormal3f, glTexCoord2f, glEnd, glDrawPixels
-// Transzformaciok: glViewport, glMatrixMode, glLoadIdentity, glMultMatrixf, gluOrtho2D, 
-// glTranslatef, glRotatef, glScalef, gluLookAt, gluPerspective, glPushMatrix, glPopMatrix,
-// Illuminacio: glMaterialfv, glMaterialfv, glMaterialf, glLightfv
-// Texturazas: glGenTextures, glBindTexture, glTexParameteri, glTexImage2D, glTexEnvi, 
-// Pipeline vezerles: glShadeModel, glEnable/Disable a kovetkezokre:
-// GL_LIGHTING, GL_NORMALIZE, GL_DEPTH_TEST, GL_CULL_FACE, GL_TEXTURE_2D, GL_BLEND, GL_LIGHT[0..7]
-//
-// NYILATKOZAT
-// ---------------------------------------------------------------------------------------------
-// Nev    : <ZUGOR> <MARTON>
-// Neptun : <MA8WE1>
-// ---------------------------------------------------------------------------------------------
-// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy 
-// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem. 
-// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a 
-// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb 
-// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem, 
-// azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
-// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.  
-// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat 
-// negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
-//=============================================================================================
-
 #include <math.h>
 #include <stdlib.h>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
-// MsWindows-on ez is kell
+// MsWindows miatt
 #include <windows.h>     
 #endif // Win32 platform
 
 #include <GL/gl.h>
 #include <GL/glu.h>
-// A GLUT-ot le kell tolteni: http://www.opengl.org/resources/libraries/glut/
+// GLUT: http://www.opengl.org/resources/libraries/glut/
 #include <GL/glut.h>     
 
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Innentol modosithatod...
 
 const float PI = 3.14159265;
 int text_id_factory = 0;
@@ -231,9 +186,9 @@ public:
 		Vector dist = helipos - eye;
 		float stretch = dist.Length() - 2.2;
 		if(stretch < 0) stretch = 0;
-		float Fpull = stretch * 10; //10 newton/m rugóállandó
+		float Fpull = stretch * 10; //10 newton/m rugoallando
 		Vector dir0 = dist.normalize();
-		Vector pull = (dir0 * Fpull) * (1.0 / 5.0); //5kg kamera
+		Vector pull = (dir0 * Fpull) * (1.0 / 5.0); //5kg camera
 
 		Vector lift = Vector(0.0, 0.05, 0.0);
 
@@ -1428,7 +1383,7 @@ void onKeyboard(unsigned char key, int x, int y) {
 // Eger esemenyeket lekezelo fuggveny
 void onMouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT && state == GLUT_DOWN)   // A GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON illetve GLUT_DOWN / GLUT_UP
-		glutPostRedisplay( ); 						 // Ilyenkor rajzold ujra a kepet
+		glutPostRedisplay( );
 }
 
 // `Idle' esemenykezelo, jelzi, hogy az ido telik, az Idle esemenyek frekvenciajara csak a 0 a garantalt minimalis ertek
@@ -1439,31 +1394,29 @@ void onIdle( ) {
 	 glutPostRedisplay();
 }
 
-// ...Idaig modosithatod
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// A C++ program belepesi pontja, a main fuggvenyt mar nem szabad bantani
+
 int main(int argc, char **argv) {
     glutInit(&argc, argv); 				// GLUT inicializalasa
-    glutInitWindowSize(600, 600);			// Alkalmazas ablak kezdeti merete 600x600 pixel 
-    glutInitWindowPosition(100, 100);			// Az elozo alkalmazas ablakhoz kepest hol tunik fel
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);	// 8 bites R,G,B,A + dupla buffer + melyseg buffer
 
-    glutCreateWindow("Grafika hazi feladat");		// Alkalmazas ablak megszuletik es megjelenik a kepernyon
+    glutCreateWindow("McRaeCopter");
 
     glMatrixMode(GL_MODELVIEW);				// A MODELVIEW transzformaciot egysegmatrixra inicializaljuk
     glLoadIdentity();
     glMatrixMode(GL_PROJECTION);			// A PROJECTION transzformaciot egysegmatrixra inicializaljuk
     glLoadIdentity();
 
-    onInitialization();					// Az altalad irt inicializalast lefuttatjuk
+    onInitialization();
 
     glutDisplayFunc(onDisplay);				// Esemenykezelok regisztralasa
     glutMouseFunc(onMouse); 
     glutIdleFunc(onIdle);
     glutKeyboardFunc(onKeyboard);
 
-    glutMainLoop();					// Esemenykezelo hurok
+    glutMainLoop();
     
     return 0;
 }
